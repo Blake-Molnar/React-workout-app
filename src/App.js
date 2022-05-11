5d15422a49820ebda29f7fdddf859c8de0be8c3d
BlakeJMolnar <blakeisnotdumb@gmail.com>
2022-05-11 18:08:06 -0500

    feat: plate count added

--- src/App.js
+++ src/App.js
@@ -5,25 +5,62 @@ import React, { useState, useEffect, useRef } from 'react';
 function App() {
 
 	const [availablePlates, setAvailablePlates] = useState([]);
-  const [workWeight, setWorkWeight] = useState(0)
-  // const workWeight = useRef(null)
+  // todo: add set barweight function
+  const [barWeight, setBarWeight] = useState(45);
+  const [calculateOutput, setCalculateOutput] = useState("")
+  const workWeight = useRef(null)
+  const hundreds = useRef(null)
+  const fortyFives = useRef(null)
+  const thirtyFives = useRef(null)
+  const twentyFives = useRef(null)
+  const fifteens = useRef(null)
+  const tens = useRef(null)
+  const fives = useRef(null)
+  const twoAndAHalf = useRef(null)
+  
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setAvailablePlates(values => ({...values, [name]: value}))
+  const setPlates = () => {
+
+    setAvailablePlates([])
+    setAvailablePlates(availablePlates => ([...availablePlates, { weight: 100, value: hundreds.current.value }]))
+    setAvailablePlates(availablePlates => ([...availablePlates, { weight: 45, value: fortyFives.current.value}]))
+    setAvailablePlates(availablePlates => ([...availablePlates, { weight: 35, value: thirtyFives.current.value}]))
+    setAvailablePlates(availablePlates => ([...availablePlates, { weight: 25, value: twentyFives.current.value}]))
+    setAvailablePlates(availablePlates => ([...availablePlates, { weight: 15, value: fifteens.current.value}]))
+    setAvailablePlates(availablePlates => ([...availablePlates, { weight: 10, value: tens.current.value}]))
+    setAvailablePlates(availablePlates => ([...availablePlates, { weight: 5, value: fives.current.value}]))
+    setAvailablePlates(availablePlates => ([...availablePlates, { weight: 2.5, value: twoAndAHalf.current.value}]))
+
   }
 
-function  calculate() {
+const calculate = () => {
+  setPlates()
+  setCalculateOutput("")
+  this.forceUpdate();
 
-  // availablePlates.forEach(pair => {
+  let weight = workWeight.current.value - barWeight
+  availablePlates.forEach(pair => {
+
     
-  // });
-  alert(availablePlates[0].name);
+    if(pair.value > 0 && weight > 0)
+    {
+      let numOfPairs = 1
+
+      for (let i = 1; i <= pair.value; i++) {
+
+        if(weight - (pair.weight * 2) >= 0)
+        {
+          weight = weight - (pair.weight * 2)
+          numOfPairs = i
+        }
+
+      }
+      setCalculateOutput(calculateOutput => calculateOutput + `${numOfPairs} ${pair.weight}'s `)
+    }
+    
+  });
 
 }
-
-
 
   return (
     <div>
@@ -33,42 +70,38 @@ function  calculate() {
           <legend>Available Plate Pairs:</legend>
 
             <label htmlFor="100">100</label>
-              <input type="number" id="100" name="100" defaultValue={0}
-              onChange={handleChange}/>
+              <input type="number" id="100" name="100" defaultValue={0} ref={hundreds}/>
 
               <label htmlFor="45">45</label>
-              <input type="number" id="45" name="45" defaultValue={2}/>
+              <input type="number" id="45" name="45" defaultValue={2} ref={fortyFives}/>
 
               <label htmlFor="35">35</label>
-              <input type="number" id="35" name="35" defaultValue={0}/>
+              <input type="number" id="35" name="35" defaultValue={0} ref={thirtyFives}/>
 
               <label htmlFor="25">25</label>
-              <input type="number" id="25" name="25" defaultValue={2}/>
+              <input type="number" id="25" name="25" defaultValue={2} ref={twentyFives} />
 
               <label htmlFor="15">15</label>
-              <input type="number" id="15" name="15" defaultValue={1}/>
+              <input type="number" id="15" name="15" defaultValue={1} ref={fifteens}/>
 
               <label htmlFor="10">10</label>
-              <input type="number" id="10" name="10" defaultValue={1}/>
+              <input type="number" id="10" name="10" defaultValue={1} ref={tens}/>
 
               <label htmlFor="5">5</label>
-              <input type="number" id="5" name="5" defaultValue={1}/>
+              <input type="number" id="5" name="5" defaultValue={1} ref={fives}/>
 
               <label htmlFor="2.5">2.5</label>
-              <input type="number" id="2.5" name="2.5" defaultValue={1}/>
+              <input type="number" id="2.5" name="2.5" defaultValue={1} ref={twoAndAHalf}/>
             
             {/* todo: add change plates later(?) */}
 
         </fieldset>
 
         <label htmlFor="weight" >Work Weight:</label>
-        <input type="number" id="weight" name="weight" />
-
-        {/* {something.map( (x) => 
-
-        x
-
-        )} */}
+        <input type="number" id="weight" name="weight" ref={workWeight}/>
+        <p>
+          {calculateOutput}
+        </p>
 
       </form>
       <button onClick={() => calculate()}>Calculate</button>
